f7c63248f7d4ea40d173a44f15db9f683cf1d930
sukonrat <48665087+sukonrat@users.noreply.github.com>
2022-04-13 16:19:56 +1000

    edited connect metamask feature

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -183,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ce452bdddb2b8a2bf25"; }
+/******/ 		__webpack_require__.h = function() { return "4e682af01eef45a66a7e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/pages/connectWallet/walletCard.tsx
+++ src/components/pages/connectWallet/walletCard.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 
 const WalletCard = () => {
+  
+  /*
   const [errorMessage, setErrorMessage] = useState(null);
   const [defaultAccount, setDefaultAccount] = useState(null);
   const [userBalance, setUserBalance] = useState(null);
@@ -16,7 +18,7 @@ const WalletCard = () => {
           accountChangedHandler(result[0]);
         });
     } else {
-      setErrorMessage('install metamask');
+      setErrorMessage("install metamask");
     }
   };
 
@@ -37,17 +39,67 @@ const WalletCard = () => {
     window.location.reload();
   }
 
- // window.ethereum.on('accountsChanged', accountChangedHandler);
+*/
 
- // window.ethereum.on('chainChange', chainChangedHandler);
+  // window.ethereum.on('accountsChanged', accountChangedHandler);
+
+  // window.ethereum.on('chainChange', chainChangedHandler);
+
+
+
+  const [walletAddress, setwalletAddress] = useState(" ");
+  const [walletBalance, setwalletBalance] = useState("");
+  const [isConnected, setIsConnected] = useState(false);
+
+  async function connect() {
+    console.log("requesting account...");
+
+    //check if metamask extension exists
+    if (typeof window.ethereum !== "undefined") {
+      console.log("metamask detected");
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setIsConnected(true);
+        setwalletAddress(accounts[0]);
+        let connectedProvider = new ethers.providers.Web3Provider(
+          window.ethereum
+        );
+      } catch (error) {
+        console.log("error connecting..");
+      }
+    } else {
+      console.log("metamask not detected");
+    }
+  }
+
+  const getWalletBalance = (address) => {
+    window.ethereum.request({method: 'eth_getBalance', params: [address, 'latest']})
+    .then(balance => {
+      setWalletBalance(ethers.utils.formatEther(balance));
+    })
+  };
+
   return (
     <>
+      {/* 
       <button onClick={connectWalletHandler}>{connButtonText}</button>
       <div>
-        <h3>Add:{defaultAccount}</h3>
+        <h3>Address:{defaultAccount}</h3>
         <h3>Bal:{userBalance}</h3>
       </div>
       {errorMessage}
+      */}
+      {isConnected ? (
+        <div>
+          <h3>Address:{walletAddress}</h3>
+          <h3>Balance:{walletBalance}</h3>
+        </div>
+      ) : (
+        <button onClick={() => connect()}>connect wallet</button>
+      )}
     </>
   );
 };
